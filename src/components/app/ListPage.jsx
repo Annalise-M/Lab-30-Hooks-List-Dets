a1e277d6a6f0d37ea0b5e182e56e93ca655c9df6
Annalise Murphy <nali247@gmail.com>
2020-10-26 19:10:57 -0700

    tests passing for listPage -console.log appearing but working on some undefined data

--- src/components/app/ListPage.jsx
+++ src/components/app/ListPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getApiAll } from '../../services/heyArnold-api';
-
+import PropTypes from 'prop-types';
 
 //ListPage Displays a list of things on the home page of application
 const ListPage = () => {
@@ -11,8 +11,18 @@ const ListPage = () => {
       .then(characters => allCharacters(characters));
   }, []);
   return (
-    <getApiAll characters={characters} />
+    <getApiAll
+      characters={characters}
+    />
   );
 };
 
+ListPage.propTypes = {
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  gifLink: PropTypes.string.isRequired
+};
+
 export default ListPage;
+
--- src/services/heyArnold-api.jsx
+++ src/services/heyArnold-api.jsx
@@ -1,21 +1,32 @@
+
 const fetch = require('node-fetch');
 
 export const getApiAll = () => {
   return fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
     .then(res => res.json())
-    .then(json => json.data.map(character => ({
-      _id: character._id,
-      name: character.author,
-      image: character.image
+    .then(json => json.map(characters => ({
+      _id: characters._id,
+      name: characters.author,
+      image: characters.image,
+      gifLink: characters.gifLink
+    })))
+    
+    .then(console.log, 'whhhhhheeeeerrrrreeeeeeeeeissssthissss');
+};
+
+export const getApiById = (_id) => {
+  // eslint-disable-next-line max-len
+  return fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51${_id}`)
+    .then(res => res.json())
+    .then(json => json.map(characters => ({
+      _id: characters._id,
+      name: characters.author,
+      image: characters.image
     })));
 };
 
-export const getApiByName = (name) => {
-  // eslint-disable-next-line max-len
-  return fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters?name=${name}`)
-    .then(res => res.json())
-    .then(json => json.data.map(character => ({
-      name: character.author,
-      image: character.image
-    })));
-};
+//https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=3&page=2
+
+//gifLink: characters.gifLink
+//https://hey-arnold-api.herokuapp.com/api/v1/gifs
+//https://hey-arnold-api.herokuapp.com/api/v1/gifs/5da23754845fd2cb76d59ee7
